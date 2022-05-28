538c238bca7e340dd06211ba38900f7b7bba869a
Igor Fastroni Correa <fastroni@192.168.1.7>
2022-05-28 08:52:08 -0700

    removendo key

--- src/server.js
+++ src/server.js
@@ -1,19 +1,5 @@
 const app = require('./app');
-const https = require('https');
+const http = require('http');
 var fs = require("fs");
 
-//https.createServer(app).listen(3333);
-
-https
-  .createServer(
-    {
-      key: fs.readFileSync("key.pem"),
-      cert: fs.readFileSync("cert.pem"),
-    },
-    app
-  )
-  .listen(3333, function () {
-    console.log(
-      "Example app listening on port 3000! Go to https://localhost:3333/"
-    );
-  });
+http.createServer(app).listen(3333);
