e62ff3715573788560720c3cc50ae43cbb864f8e
Igor Fastroni Correa <fastroni@MacBook-Pro-de-Igor.local>
2022-06-26 22:46:19 -0300

    database

--- src/app/database/index.js
+++ src/app/database/index.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 mongoose.set('useCreateIndex', true);
-//mongoose.connect('mongodb+srv://dbuser:mobot@pass!123@cluster0.2sros.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://dbuser:mobot@pass!123@cluster0.2sros.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 
-mongoose.connect('mongodb://localhost/mobot', { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect('mongodb://localhost/mobot', { useNewUrlParser: true, useUnifiedTopology: true });
 
 mongoose.Promise = global.Promise;
 
