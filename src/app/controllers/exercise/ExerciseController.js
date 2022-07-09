97f8e53b056825c6d760ee8c4958c14719d96ed2
Igor Fastroni Correa <fastroni@MacBook-Pro-de-Igor.local>
2022-07-09 17:54:44 -0300

    exercicios com audio no s3

--- src/app/controllers/exercise/ExerciseController.js
+++ src/app/controllers/exercise/ExerciseController.js
@@ -19,28 +19,26 @@ exports.postCreateExercise = async (req, res) => {
         objectExercise = { ...req.body, fonoId, exampleAudioUrl };
 
         var data = await repository.create(objectExercise);
-
-        var pathToSourceFile = path.resolve(__dirname, `../../../../exercises/${data._id}-exercise.mp3`);
-
-        const fileContent = await fs.promises.readFile(pathToSourceFile);
-
-        const contentType = mime.getType(pathToSourceFile);
-
+        const idExercise = data._id;
 
         fs.writeFile(`exercises/${data._id}-exercise.mp3`, base64String.replace("data:audio/mp3;base64,", ""), { encoding: 'base64' }, async function (err) {
             console.log('File mp3 created');
 
+            var pathToSourceFile = path.resolve(__dirname, `../../../../exercises/${idExercise}-exercise.mp3`);
+            const fileContent = await fs.promises.readFile(pathToSourceFile);
+            const contentType = mime.getType(pathToSourceFile);
+            console.log(idExercise)
             const s3 = new AWS.S3();
             await s3.putObject({
                 Body: fileContent,
-                Key: data._id,
+                Key: idExercise + "-exercise.mp3",
                 Bucket: "mobot-audios",
                 ContentType: contentType,
                 ACL: 'public-read',
             }).promise();
         });
 
-        exampleAudioUrl = `https://mobot-audios.s3.us-west-2.amazonaws.com/${data._id}-exercise.mp3`;
+        exampleAudioUrl = `https://mobot-audios.s3.us-west-2.amazonaws.com/${idExercise}-exercise.mp3`;
 
         await repository.put(data._id, exampleAudioUrl)
 
